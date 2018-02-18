1035be9e8caea381b4719df3d83a4532f2ae07f6
Nathan Burgo <nthnbrg185@gmail.com>
2018-02-17 21:17:35 -0700

    added ability for user to input number of rows and columns of pixels and generate grid. Palette/user input section added on left and grid on right

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded', function (){
-  var body = document.querySelector('body');
+  // define variables
+  var xInput = document.getElementById('x-input');
+  var yInput = document.getElementById('y-input');
   var gridBody = document.getElementsByClassName('grid')[0];
   var gridRow = null;
   var pixel = null;
   var palette = document.getElementsByClassName('palette')[0];
   var currentColor;
-
-  var createPixels = function (x, y){
-    for (var i = 0; i < y; i++){
+  var submit = document.querySelector('button');
+  // Create a function that takes the user's height and width inputs and generates a grid and on clicking the submit button, the grid generates
+  submit.addEventListener('click', function (){
+    if (xInput.value > 70 || yInput.value > 100 ){
+      return alert('Please choose a smaller grid size');
+    }
+    for (var i = 0; i < yInput.value; i++){
       gridRow = document.createElement('div');
       gridRow.className = 'grid-row';
       gridBody.append(gridRow);
-      for (var j = 0; j < x; j++){
+      for (var j = 0; j < xInput.value; j++){
         pixel = document.createElement('div');
         pixel.className = 'pixel'
         gridRow.append(pixel);
       }
     }
-  }
-
-  createPixels(10, 100);
+  })
 
   palette.addEventListener('click', function(event){
     currentColor = event.target.classList[0];
